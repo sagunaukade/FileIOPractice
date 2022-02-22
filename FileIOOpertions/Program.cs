bcd723a2f6c2004d68ea8f560349fdaf2127b577
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-22 17:27:17 +0530

    [Saguna] File IO operations

---
+++ FileIOOpertions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOOpertions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the basic io operations");
+            FileOperations fileoperation = new FileOperations();
+            fileoperation.fileExits();
+            fileoperation.readAllLines();
+            fileoperation.readAllText();
+            fileoperation.fileCopy();
+            StreamReaderWriterOp stream = new StreamReaderWriterOp();
+            stream.ReadFromStreamReader();
+            stream.WriteFromStreamWriter();
+            Console.ReadKey();
+        }
+    }
+}
