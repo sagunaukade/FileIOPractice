e3fb47d9142a26e5fb9cafe8307777ae9f54384e
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-22 18:11:41 +0530

    [Saguna] Serialization And Deserialization

---
+++ SerializationAndDeserialization/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationAndDeserialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Serialization And Deserialization program ");
+            BinarySerialization binary = new BinarySerialization();
+            binary.Serialization();
+            Console.ReadKey();
+        }
+    }
+}
